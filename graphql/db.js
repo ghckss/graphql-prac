97a4378411def2ad09fb167edc7318ef368dfb5d
hwanghochan <ghckss93@gmail.com>
2018-11-12 00:06:12 +0900

    "graphql with movie end"

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "0",
-    name: "hochan",
-    age: 24,
-    gender: "male"
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "1",
-    name: "ho",
-    age: 22,
-    gender: "male"
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "2",
-    name: "chan",
-    age: 23,
-    gender: "male"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
+  },
+  {
+    id: 3,
+    name: "Login",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
